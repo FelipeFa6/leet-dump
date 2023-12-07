8b4bbf120f5576eff496b3252d0af37e30294a11
FelipeFa6 <felipe.farias.e1@gmail.com>
2023-12-07 00:06:31 -0300

    Initial Commit

---
+++ fizzbuzz/main.rs
@@ -0,0 +1,23 @@
+fn fizz_buzz(x: u8) -> String{
+
+    let mut resp = String::new();
+    if x%3 == 0 && x%5 == 0 {
+        resp = resp + "FizzBuzz";
+    }
+    else if x%3 == 0 {
+        resp = resp + "Fizz";
+    }
+    else if x%5 == 0 {
+        resp = resp +  "Buzz";
+    }
+
+    return resp;
+}
+
+
+fn main() {
+    // FizzBuzz implementation
+    for x in 1..=101 {
+        println!("{x} {}", fizz_buzz(x));
+    }
+}
